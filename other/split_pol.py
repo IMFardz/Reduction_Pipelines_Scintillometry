ef28998512404f1d23774dc1a0fb4eb0d6003d36
Fardin Syed <fardin.syed99@gmail.com>
2019-03-15 18:01:03 -0400

    Completed polarization splitting code

--- other/split_pol.py
+++ other/split_pol.py
@@ -3,35 +3,33 @@ from baseband import vdif
 import astropy.units as u
 import sys
 
-path = '/scratch/p/pen/simardda/B1133/gk049c/gb/'
+# File path of basbeand data
+path = '/Users/fardinsyed/Desktop/baseband/baseband/data/'
 filename = sys.argv[1]
-thread_ids = np.arange(8)
-outnames=[filename+'_pol_'+str(i) for i in range(2)]
-
-# Characteristics of Data
-nthread=8
-nchan=1
-
+outnames = [filename + '_pol_' + str(i) for i in range(2)]
 
 try:
-    fh = vdif.open(path+filename+'.vdif','rs',sample_rate=sample_rate,nthread=nthread,nchan=nchan)
+    # Open stream reader and obtain file size
+    fh = vdif.open(path+filename+'.vdif','rs')
     file_size = fh.fh_raw.seek(0, 2) // 2
     fh.fh_raw.seek(0)
     size = fh.seek(-1, 2)
     fh.seek(0)
 
-    # Create stream reader for each polarization
-    fos = [vdif.open(outname+'.vdif','ws',sample_rate=fh.sample_rate,nthread=4,nchan=1,
-    samples_per_frame=fh.sample_rate, file_size=file_size) for outname in outnames]
+    # Create stream writer for each polarization
+    fos = [vdif.open(outname+'.vdif','ws',sample_rate=fh.sample_rate,nthread=4,
+    nchan=1,samples_per_frame=fh.samples_per_frame, file_size=file_size) for outname in outnames]
 
-    # Find each thread and make a vdif file for each polarization
+    # Write into output streams frame by frame
     while fh.fh_raw.tell() < size:
         arr = fh.fh_raw.read_frame()
         arr.header.mutable = True
-        old_thread = arr.header
-        arr.header['thread_id'] = old_header // 2
+        old_thread = arr.header['thread_id']
+        arr.header.update(thread_id = old_thread // 2)
         arr.header.mutable = False
-        fos[old_thread % 2].write_frame(arr)
+        fos[old_thread % 2].fh_raw.write_frame(arr)
+
+# Trying my best not to corrupt anything
 except KeyboardInterrupt:
     print('Closing files')
     fh.close()
@@ -39,6 +37,7 @@ except KeyboardInterrupt:
         fos[i].close()
     sys.exit()
 
+# Terminating
 fh.close()
 for i in range(len(fos)):
     fos[i].close()
